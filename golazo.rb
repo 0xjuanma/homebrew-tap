543afd85ddd4f7575146b786e1913a0543b29e62
Juanma Roca <juanmaxroca@gmail.com>
2026-01-31 17:18:05 -0500

    chore[golazo]: update sha256 for v0.18.0

--- golazo.rb
+++ golazo.rb
@@ -2,27 +2,27 @@ class Golazo < Formula
   desc "See football match stats in real-time in your terminal"
   homepage "https://github.com/0xjuanma/golazo"
 
-  version "0.17.0"
+  version "0.18.0"
 
   on_macos do
     on_arm do
       url "https://github.com/0xjuanma/golazo/releases/download/v#{version}/golazo-darwin-arm64"
-      sha256 "5d8766c42a42107180e2a09db4cdf4cb4f564f79bb9942ce5decb5bcafd4bc9b"
+      sha256 "7e05bbf4386613fcff341a6812c149293ef4adaca1c3fb28251925be76a53609"
     end
     on_intel do
       url "https://github.com/0xjuanma/golazo/releases/download/v#{version}/golazo-darwin-amd64"
-      sha256 "e79123ec875d1359582b90cdb4cec747b3d8a529a966a77e58c32b73c2d543b6"
+      sha256 "4d57bd567b6498cd0de13d1d6510c40ba32393800a6f45e447baa08593850792"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/0xjuanma/golazo/releases/download/v#{version}/golazo-linux-arm64"
-      sha256 "8ad53f25919943ad7f664491835f396d7970f7c04bdd5f766780aecc06511b36"
+      sha256 "cf1698e5ee3e054d49a376c0e75c3be03b442b89b31e6bd25a6af24e23a0cd5b"
     end
     on_intel do
       url "https://github.com/0xjuanma/golazo/releases/download/v#{version}/golazo-linux-amd64"
-      sha256 "2d213c4cf3216f06d36990871127f7339843304b3ddebc6409cac8af03ab2543"
+      sha256 "f6507ea4b5a4d328784d9f36bd347c93e7420c0a5e2e1f688d6485d22eadc9e2"
     end
   end
 
